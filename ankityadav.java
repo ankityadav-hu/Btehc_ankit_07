94ca4c5e8f83e344208907cd0f5d9aed0434b904
ankityadav-hu <20.akyadv@gmail.com>
2026-02-10 16:54:59 +0530

    Add files via upload

---
+++ ankityadav.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class ankityadav {
+    public static void main(String args[]) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number a: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter number b: ");
+        int b = scanner.nextInt();
+
+        System.out.println( "Sum = " + (a + b));
+        System.out.println( "diff = " + (a - b));
+
+
+
+    }
+    
+}
